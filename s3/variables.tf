53159ec434572a4b8ac9eacde80e27e53889f42d
Aline Morelli <aline.morelliw@gmail.com>
2018-08-10 14:10:58 -0300

    feat: add s3 and cloudfront modules

---
+++ s3/variables.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "name" {}
+variable "type" {}
+variable "env" {}
+variable "team" {}
+variable "account_id" {}
+variable "s3_bucket_name" {}
+variable "s3_acl" {}
+variable "s3_versioning" {}
+variable "s3_policy" {}
+variable "s3_prevent_destroy" {}
