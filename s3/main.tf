c65db579070a7181f62ed13286d9084aec821269
Aline Morelli <amorelli@aliancahospitalar.com>
2018-08-10 18:08:39 -0300

    fix: change s3 prevent-destroy to bool

--- s3/main.tf
+++ s3/main.tf
@@ -2,8 +2,11 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "${var.s3_bucket_name}"
   acl    = "${var.s3_acl}"
   lifecycle {
-      prevent_destroy = "${var.prevent_destroy}"
+    prevent_destroy = false
   }
+
+  force_destroy = true
+  
   policy = "${file("${var.s3_policy}")}"
   tags {
         Name = "${var.name}"
