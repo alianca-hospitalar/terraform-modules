e820db58260eac400d242b75cb146a0d1deadadb
Daniel Baptista Dias <danielbpdias@gmail.com>
2018-08-21 19:20:43 -0300

    Simplifying some Cloudfront variables

--- cloudfront-s3/main.tf
+++ cloudfront-s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "static-${var.name}-${var.env}"
+    bucket = "${var.s3_bucket}"
     acl = "${var.s3_acl}"
     lifecycle {
       prevent_destroy = false
@@ -13,11 +13,11 @@ resource "aws_s3_bucket" "s3_bucket" {
     website {
       index_document = "index.html"
       error_document = "index.html"
-      routing_rules = "${var.routing_rules}"
+      routing_rules = "${var.s3_routing_rules}"
     }
     tags {
-        Name = "${var.name}"
-        Environment = "${var.env}"
+      Environment = "${var.environment}"
+      System = "${var.system}"
     }
 }
 
@@ -25,11 +25,11 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   depends_on = [ "aws_s3_bucket.s3_bucket" ]
   origin {
     domain_name = "${aws_s3_bucket.s3_bucket.website_endpoint}"
-    origin_id   = "static-${var.name}-${var.env}-dist-id"
+    origin_id   = "${var.s3_bucket}-dist-id"
     custom_origin_config {
       http_port = 80
       https_port = 443
-      origin_protocol_policy = "${var.origin_protocol_policy}"
+      origin_protocol_policy = "${var.cf_origin_protocol_policy}"
       origin_ssl_protocols = [ "SSLv3", "TLSv1" ]
     }
   }
@@ -40,8 +40,8 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   default_cache_behavior {
     allowed_methods  = [ "HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH" ]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "static-${var.name}-${var.env}-dist-id"
-    compress = "${var.cloudfront_compress}"
+    target_origin_id = "${var.s3_bucket}-dist-id"
+    compress = "${var.cf_compress}"
 
     forwarded_values {
       query_string = false
@@ -50,7 +50,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
       }
     }
 
-    viewer_protocol_policy = "${var.viewer_protocol_policy}"
+    viewer_protocol_policy = "${var.cf_viewer_protocol_policy}"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -63,9 +63,9 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     }
 
   viewer_certificate {
-    cloudfront_default_certificate = "${var.cloudfront_default_certificate}"
-    acm_certificate_arn = "${var.acm_certificate_arn}"
-    minimum_protocol_version = "${var.minimum_protocol_version}"
-    ssl_support_method = "${var.ssl_support_method}"
+    cloudfront_default_certificate = "${var.cf_default_certificate}"
+    acm_certificate_arn = "${var.cf_acm_certificate_arn}"
+    minimum_protocol_version = "${var.cf_minimum_protocol_version}"
+    ssl_support_method = "${var.cf_ssl_support_method}"
   }
 }
--- cloudfront-s3/variables.tf
+++ cloudfront-s3/variables.tf
@@ -2,49 +2,68 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "name" {}
-variable "env" {}
-variable "team" {}
-variable "key_name" {}
-variable "account_id" {}
-variable "cf_aliases" {
-        type = "list"
-        default = []
-}
-variable "domain" {
-		default = ""
+/***** Common variables *****/
+
+/* Type of the environment where the resources will be created. (e.g. production, qa (quality assurance), etc.) */
+variable "environment" {
+  default = "production"
 }
 
+/* Name of the system that owns this resources */
+variable "system" {}
+
+/***** S3 settings variables *****/
+
+/* Name of the S3 bucket with the sources that will be exposed in the Cloudfront CDN */
+variable "s3_bucket" {}
+
+/* ACL rules for the S3 bucket exposed by Cloudfront */
 variable "s3_acl" {
-        default = "public-read"
+  default = "public-read"
 }
 
-variable routing_rules {
+/* Routing rules that will be used */
+variable s3_routing_rules {
     default = ""
 }
 
-variable viewer_protocol_policy {
-    default = "allow-all"
+/***** Cloudfront variables *****/
+
+/* The origin protocol policy to apply to your cloudfront origin. The possible values are: http-only, https-only, or match-viewer */
+variable cf_origin_protocol_policy {}
+
+/* Extra CNAMEs (alternate domain names), if any, for this Cloudfront distribution */
+variable "cf_aliases" {
+  type = "list"
+  default = []
 }
 
-variable origin_protocol_policy {}
+/* Tell to Cloudfront if you want that the web requests have its data automatically compressed when a request header "Accept-Encoding: gzip" is found  */
+variable cf_compress {
+  default = "true"
+}
 
-variable cloudfront_default_certificate {
+/* Specify the protocol that users can use to access the files in the origin. Possible values: allow-all, https-only, or redirect-to-https */
+variable cf_viewer_protocol_policy {
+  default = "allow-all"
+}
+
+/* Flag indicating if the default Cloudfront certificate must be used */
+variable cf_default_certificate {
+  default = ""
+}
+
+/* ARN of the ACM certificate to be used in this CDN */
+variable cf_acm_certificate_arn {
     default = ""
 }
-    
-variable acm_certificate_arn {
-    default = ""
-}
 
-variable minimum_protocol_version {
+/* Minimum version of the SSL protocol that you want CloudFront to use for HTTPS connections */
+variable cf_minimum_protocol_version {
     default = "SSLv3"
 }
 
-variable ssl_support_method {
+/* Specifies how you want CloudFront to serve HTTPS requests. One of vip or sni-only */
+variable cf_ssl_support_method {
     default = "sni-only"
-}
-
-variable cloudfront_compress {
-    default = "true"
 }
